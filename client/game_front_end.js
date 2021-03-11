aba0262ffd9c781f58b4773265b1b62f8300ccff
Emre Cenk <emrecenk9@gmail.com>
2021-03-11 15:41:51 -0500

    set up a mini chat app for testing the socket connection

--- client/game_front_end.js
+++ client/game_front_end.js
@@ -1,7 +1,17 @@
 
 
-const socket = io("http://localhost:5000");
-socket.on("init",initialize);
-function initialize(message){
-    console.log(message);
-}
+
+
+$(document).ready(
+    function(){
+        //connecting to socket: 
+        var socket = io.connect("http://127.0.0.1:5000/")
+
+
+        socket.on("connect",function(){
+            socket.send("user has connected");
+        });
+
+
+    }
+)
