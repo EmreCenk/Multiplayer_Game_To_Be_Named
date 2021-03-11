2b0ae0ecafa0115ae9d7e279231be3c76bff85f8
Emre Cenk <emrecenk9@gmail.com>
2021-03-11 13:36:27 -0500

    setting up flask

--- server/back_end.py
+++ server/back_end.py
@@ -1,1 +1,15 @@
 
+
+
+
+from flask import Flask
+from flask_socketio import SocketIO, send
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "I will eventually set a secret key"
+
+socketio = SocketIO(app)
+
+
+if __name__ == "__main__":
+    socketio.run(app)
