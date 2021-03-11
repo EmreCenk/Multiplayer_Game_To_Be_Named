bc787a60bcc550f1dbe4f49f8517f5450d678e2a
Emre Cenk <emrecenk9@gmail.com>
2021-03-11 16:26:59 -0500

    moving character

--- client/game_front_end.js
+++ client/game_front_end.js
@@ -1,17 +1,118 @@
 
+//ESTABLISHING SOCKET CONNECTION:
+// $(document).ready(function(){
+//     var socket = io.connect("http://127.0.0.1:5000/");
+//     socket.on("connect",function(){
+//         socket.send("User has connected.");
+//     });
+
+//     socket.on("message", function(msg){
+//         $("body").append("<li>" + msg);
+//     })
+// })
+
+//CONTROLS AND ALTERNATIVES
+const move_left = ['ArrowLeft','a']; // the 'a' and left arrow
+const move_right = ['ArrowRight','d']; // 'd' and right arrow
+const move_up = ['ArrowUp','w']; // 'w' and up arrow
+const move_down = ['ArrowDown','s']; //'s' and down arrow
+
+var moving = [false,false,false,false]; // index 0: left ,1:right , 2:up, 3:down
 
 
+const generate_coordinates = (0,0);
+var canvas = document.querySelector("canvas") //selecting the canvas from the html
+var c = canvas.getContext("2d");
+//Making canvas full screen:
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-$(document).ready(
-    function(){
-        //connecting to socket: 
-        var socket = io.connect("http://127.0.0.1:5000/")
 
+class character{
+    constructor(x,y,radius, vx, vy, color="black"){
+        this.x=x;
+        this.y=y;
+        this.radius = radius;
+        this.vx=vx;
+        this.vy = vy;
+        this.color=color;
+    }
 
-        socket.on("connect",function(){
-            socket.send("user has connected");
-        });
-
+    move(array_of_current_moves){
+        if (array_of_current_moves[0]){
+            this.x-=this.vx;
+        }
+        
+        if (array_of_current_moves[1]){
+            this.x+=this.vx;
+        }
+        
+        if (array_of_current_moves[2]){
+            this.y-=this.vy;
+        }
+        
+        if (array_of_current_moves[3]){
+            this.y+=this.vy;
+        }
 
     }
-)
+    draw(){
+        c.beginPath();
+        c.arc(this.x,this.y,this.radius,0,Math.PI*2);
+        c.fillStyle = this.color;
+        c.fill();
+
+    }
+}
+
+var main_ch = new character(0,0,5,5,5);
+
+
+window.addEventListener("keydown",key_pressed);
+window.addEventListener("keyup",key_up);
+
+function modify(cur_key, what_to=true){
+
+
+    if (move_left.includes(cur_key)){
+        moving[0] = what_to;
+    }
+    else if (move_right.includes(cur_key)){
+        moving[1] = what_to;
+    }
+    else if (move_up.includes(cur_key)){
+        moving[2] = what_to;
+    }
+    else if (move_down.includes(cur_key)){
+        moving[3] = what_to;
+    }
+
+}
+
+
+function key_pressed(event){
+
+    let cur_key = event.key;
+    modify(event.key,true)
+}
+
+function key_up(event){
+    modify(event.key,false)
+}
+
+
+
+
+
+function animate(){
+    //animate
+    //animate info received from server
+    c.clearRect(0,0,window.innerWidth,window.innerHeight);
+
+    //animate character:
+    requestAnimationFrame(animate);
+    main_ch.draw();
+    main_ch.move(moving);
+}
+
+animate();
